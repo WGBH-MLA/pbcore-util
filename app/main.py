d228173dc08aad50f9e3707689bb58b42cac5f5b
Andrew Myers <afredmyers@gmail.com>
2025-06-23 11:48:49 -0400

    Formatting using 'black' formatting tool

--- app/main.py
+++ app/main.py
@@ -11,26 +11,26 @@ XSD_PATH = "schemas/pbcore-2.1.xsd"
 XSL_PATH = "stylesheets/pbcore-xml-to-json.xsl"
 JSON_SCHEMA_PATH = "schemas/pbcore-schema.json"
 
+
 @app.post("/validate/xml-file", tags=["XML Validation"])
 async def validate_xml(file: UploadFile = File(...)):
     try:
         pbcore_xml = etree.parse(file.file)
-        pbcore_schema = etree.XMLSchema(
-            etree.parse(XSD_PATH)
-        )
+        pbcore_schema = etree.XMLSchema(etree.parse(XSD_PATH))
         pbcore_schema.assertValid(pbcore_xml)
-        return {
-            "valid": True,
-            "file": file.filename
-        }
+        return {"valid": True, "file": file.filename}
     except etree.DocumentInvalid as e:
-        raise HTTPException(status_code=422, detail=f"PBCore XML Validation Error: {str(e)}")
+        raise HTTPException(
+            status_code=422, detail=f"PBCore XML Validation Error: {str(e)}"
+        )
     except etree.XMLSyntaxError as e:
         raise HTTPException(status_code=422, detail=f"XML Parsing Error: {str(e)}")
 
 
 @app.post("/validate/xml-url", tags=["XML Validation"])
-async def validate_xml_from_url(url: str = Query(..., description="URL pointing to a PBCore XML document")):
+async def validate_xml_from_url(
+    url: str = Query(..., description="URL pointing to a PBCore XML document")
+):
     try:
         async with httpx.AsyncClient(timeout=10) as client:
             response = await client.get(url)
@@ -40,9 +40,7 @@ async def validate_xml_from_url(url: str = Query(..., description="URL pointing 
 
     try:
         pbcore_xml = etree.fromstring(response.content)
-        pbcore_schema = etree.XMLSchema(
-            etree.parse(XSD_PATH)
-        )
+        pbcore_schema = etree.XMLSchema(etree.parse(XSD_PATH))
         pbcore_schema.assertValid(pbcore_xml)
     except etree.XMLSchemaError as e:
         raise HTTPException(status_code=422, detail=f"XML Validation Error: {str(e)}")
@@ -55,7 +53,6 @@ async def validate_xml_from_url(url: str = Query(..., description="URL pointing 
     }
 
 
-
 @app.post("/validate/json-file", tags=["JSON Validation"])
 async def validate_json(file: UploadFile = File(...)):
     try:
@@ -63,16 +60,15 @@ async def validate_json(file: UploadFile = File(...)):
         with open(JSON_SCHEMA_PATH) as schema_file:
             schema = json.load(schema_file)
         json_validate(instance=instance, schema=schema)
-        return {
-            "valid": True,
-            "file": file.filename
-        }
+        return {"valid": True, "file": file.filename}
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=e.message)
 
 
 @app.post("/validate/json-url", tags=["JSON Validation"])
-async def validate_xml_from_url(url: str = Query(..., description="URL pointing to a PBCore JSON document")):
+async def validate_xml_from_url(
+    url: str = Query(..., description="URL pointing to a PBCore JSON document")
+):
     try:
         async with httpx.AsyncClient(timeout=10) as client:
             response = await client.get(url)
@@ -111,7 +107,9 @@ async def convert_xml_to_json(file: UploadFile = File(...)):
 
 
 @app.post("/convert/xml-to-json-url", tags=["Conversion"])
-async def convert_xml_to_json(url: str = Query(..., description="URL pointing to a PBCore XML document")):
+async def convert_xml_to_json(
+    url: str = Query(..., description="URL pointing to a PBCore XML document")
+):
     try:
         async with httpx.AsyncClient(timeout=10) as client:
             response = await client.get(url)
