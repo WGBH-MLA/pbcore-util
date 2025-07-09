b2781d92e984338d58c5e1aa690e097f2e2aca19
Andrew Myers <afredmyers@gmail.com>
2025-07-09 15:39:22 -0400

    Rewords OpenAPI tags for consistency

--- app/main.py
+++ app/main.py
@@ -68,7 +68,7 @@ async def validate_json_from_url(
     )
 
 
-@app.post("/convert/xml-to-json-file", tags=["Conversion"])
+@app.post("/convert/xml-to-json-file", tags=["XML to JSON Conversion"])
 async def convert_xml_to_json_from_file(file: UploadFile = File(...)):
     try:
         pbcore_xml = etree.parse(file.file)
@@ -80,7 +80,7 @@ async def convert_xml_to_json_from_file(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/convert/xml-to-json-url", tags=["Conversion"])
+@app.post("/convert/xml-to-json-url", tags=["XML to JSON Conversion"])
 async def convert_xml_to_json_from_url(
     url: str = Query(..., description="URL pointing to a PBCore XML document")
 ):
@@ -101,14 +101,14 @@ async def convert_xml_to_json_from_url(
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/convert/json-to-xml", tags=["PBCore Conversion"])
+@app.post("/convert/json-to-xml", tags=["JSON to XML Conversion"])
 async def convert_json_to_xml(file: UploadFile = File(...)):
     raise HTTPException(
         status_code=400, detail="PBCore JSON to XML Conversion not yet implemented"
     )
 
 
-@app.post("/convert/roundtrip/xml-json/file", tags=["PBCore Conversion"])
+@app.post("/convert/roundtrip/xml-json/file", tags=["JSON to XML Conversion"])
 async def validate_roundtrip(file: UploadFile = File(...)):
     raise HTTPException(
         status_code=400,
